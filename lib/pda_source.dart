7b8fec54d229a15ee59355222ab24f47db5fefd5
Quentin Gallois <guaire94.perso@gmail.com>
2021-09-27 17:48:55 +0400

    update to null safety way

--- lib/pda_source.dart
+++ lib/pda_source.dart
@@ -5,8 +5,8 @@ import 'package:pda_scanner/pda_listener_mixin.dart';
 
 class PdaSource {
   static const String channelName = 'com.shinow.pda_scanner/plugin';
-  static EventChannel _scannerPlugin;
-  static StreamSubscription _subscription;
+  static EventChannel? _scannerPlugin;
+  static StreamSubscription? _subscription;
 
   static List<PdaListenerMixin> listeners = [];
 
@@ -14,9 +14,7 @@ class PdaSource {
   static void init() {
     if (_scannerPlugin == null)
       _scannerPlugin = const EventChannel(channelName);
-    _subscription = _scannerPlugin
-        .receiveBroadcastStream()
-        .listen(_onEvent, onError: _onError);
+    _subscription = _scannerPlugin!.receiveBroadcastStream().listen(_onEvent, onError: _onError);
   }
 
   static void registerListener(PdaListenerMixin listener) {
@@ -31,10 +29,10 @@ class PdaSource {
   static void dispose() {
     listeners.clear();
     assert(_subscription != null);
-    _subscription.cancel();
+    _subscription?.cancel();
   }
 
-  static void _onEvent(Object code) {
+  static void _onEvent(dynamic code) {
     listeners.forEach((listener) => listener.checkRouteAndFireEvent(code));
   }
 
